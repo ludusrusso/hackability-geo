0be70a053e318534049ca4697c4bba626a55bebd
Ludovico Russo <ludus.russo@gmail.com>
2019-03-19 19:14:21 +0000

    up

---
+++ launch.sh
@@ -0,0 +1,4 @@
+cd /home/pi/hackability-geo/
+
+/home/pi/geo/env/bin/python /home/pi/hackability-geo/main.py
+
--- main.py
+++ main.py
@@ -3,9 +3,13 @@ from place import Place
 import json
 from gpiozero import Button
 from time import sleep
+from os import system
 
 pygame.init()
 pygame.mixer.init()
+
+def set_volume(vol):
+    system("amixer set PCM {}%".format(vol))
 
 with open('geo.json') as f:
     data = json.load(f)
@@ -17,6 +21,10 @@ for place in data['folders']:
 
 btn = Button(data['stop'])
 btn.when_pressed = places[0].stop
+
+vol = data['volume']
+set_volume(vol)
+
 
 while True:
     sleep(10)
